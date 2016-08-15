b4098cc74e9e62c26b30448de3d56012b928f545
Sebastian Klein <sebastian@sklein-medien.de>
2016-08-15 13:56:48 +0200

    Sort select options by title

--- Classes/Domain/Repository/OptionRecordRepository.php
+++ Classes/Domain/Repository/OptionRecordRepository.php
@@ -31,6 +31,7 @@ namespace Sebkln\Ajaxselectlist\Domain\Repository;
  */
 class OptionRecordRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
 {
-
-    
+    protected $defaultOrderings = array(
+            'title' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING
+    );
 }
