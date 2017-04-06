1be963be8f776680b53d72fb07cf4c40b276d020
Sebastian Klein <sebastian@sklein-medien.de>
2017-04-06 22:36:43 +0200

    Use spaces and short array syntax

--- Configuration/TCA/tx_ajaxselectlist_domain_model_optionrecord.php
+++ Configuration/TCA/tx_ajaxselectlist_domain_model_optionrecord.php
@@ -1,202 +1,202 @@
 <?php
-return array(
-	'ctrl' => array(
-		'title'	=> 'LLL:EXT:ajaxselectlist/Resources/Private/Language/locallang_db.xlf:tx_ajaxselectlist_domain_model_optionrecord',
-		'label' => 'title',
-		'tstamp' => 'tstamp',
-		'crdate' => 'crdate',
-		'cruser_id' => 'cruser_id',
-		'dividers2tabs' => true,
-		'versioningWS' => true,
+return [
+    'ctrl' => [
+        'title' => 'LLL:EXT:ajaxselectlist/Resources/Private/Language/locallang_db.xlf:tx_ajaxselectlist_domain_model_optionrecord',
+        'label' => 'title',
+        'tstamp' => 'tstamp',
+        'crdate' => 'crdate',
+        'cruser_id' => 'cruser_id',
+        'dividers2tabs' => true,
+        'versioningWS' => true,
 
-		'languageField' => 'sys_language_uid',
-		'transOrigPointerField' => 'l10n_parent',
-		'transOrigDiffSourceField' => 'l10n_diffsource',
-		'delete' => 'deleted',
-		'enablecolumns' => array(
-			'disabled' => 'hidden',
-			'starttime' => 'starttime',
-			'endtime' => 'endtime',
-		),
-		'searchFields' => 'title,image,text,',
-		'iconfile' => 'EXT:ajaxselectlist/Resources/Public/Icons/tx_ajaxselectlist_domain_model_optionrecord.gif'
-	),
-	'interface' => array(
-		'showRecordFieldList' => 'sys_language_uid, l10n_parent, l10n_diffsource, hidden, title, image, text',
-	),
-	'types' => array(
-		'0' => array(
-			'showitem' => '
-				--div--;LLL:EXT:lang/locallang_core.xlf:labels.generalTab,
-					title,image,text,
-				--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.language,
-					--palette--;;language,
-				--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.tabs.access,
-					--palette--;;visibility,
-					--palette--;;timeRestriction,
-			'
-		),
-		'columnsOverrides' => array(
-			'sys_language_uid' => array(
-				'defaultExtras' => ';;;;1-1-1'
-			),
-		),
-	),
-	'palettes' => array(
-		'visibility' => ['showitem' => 'hidden'],
-		'timeRestriction' => ['showitem' => 'starttime, endtime'],
-		'language' => ['showitem' => 'sys_language_uid, l10n_parent'],
-	),
-	'columns' => array(
-		'sys_language_uid' => array(
-			'exclude' => 1,
-			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.language',
-			'config' => array(
-				'type' => 'select',
-				'renderType' => 'selectSingle',
-				'special' => 'languages',
-				'items' => array(
-					array(
-						'LLL:EXT:lang/locallang_general.xlf:LGL.allLanguages',
-						-1,
-						'flags-multiple'
-					),
-				),
-				'default' => 0,
-			),
-		),
-		'l10n_parent' => array(
-			'displayCond' => 'FIELD:sys_language_uid:>:0',
-			'exclude' => 1,
-			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.l18n_parent',
-			'config' => array(
-				'type' => 'select',
-				'renderType' => 'selectSingle',
-				'items' => array(
-					array('', 0),
-				),
-				'foreign_table' => 'tx_ajaxselectlist_domain_model_optionrecord',
-				'foreign_table_where' => 'AND tx_ajaxselectlist_domain_model_optionrecord.pid=###CURRENT_PID### AND tx_ajaxselectlist_domain_model_optionrecord.sys_language_uid IN (-1,0)',
-			),
-		),
-		'l10n_diffsource' => array(
-			'config' => array(
-				'type' => 'passthrough',
-			),
-		),
+        'languageField' => 'sys_language_uid',
+        'transOrigPointerField' => 'l10n_parent',
+        'transOrigDiffSourceField' => 'l10n_diffsource',
+        'delete' => 'deleted',
+        'enablecolumns' => [
+            'disabled' => 'hidden',
+            'starttime' => 'starttime',
+            'endtime' => 'endtime',
+        ],
+        'searchFields' => 'title,image,text,',
+        'iconfile' => 'EXT:ajaxselectlist/Resources/Public/Icons/tx_ajaxselectlist_domain_model_optionrecord.gif'
+    ],
+    'interface' => [
+        'showRecordFieldList' => 'sys_language_uid, l10n_parent, l10n_diffsource, hidden, title, image, text',
+    ],
+    'types' => [
+        '0' => [
+            'showitem' => '
+                --div--;LLL:EXT:lang/locallang_core.xlf:labels.generalTab,
+                    title,image,text,
+                --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.language,
+                    --palette--;;language,
+                --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.tabs.access,
+                    --palette--;;visibility,
+                    --palette--;;timeRestriction,
+            '
+        ],
+        'columnsOverrides' => [
+            'sys_language_uid' => [
+                'defaultExtras' => ';;;;1-1-1'
+            ],
+        ],
+    ],
+    'palettes' => [
+        'visibility' => ['showitem' => 'hidden'],
+        'timeRestriction' => ['showitem' => 'starttime, endtime'],
+        'language' => ['showitem' => 'sys_language_uid, l10n_parent'],
+    ],
+    'columns' => [
+        'sys_language_uid' => [
+            'exclude' => 1,
+            'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.language',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'special' => 'languages',
+                'items' => [
+                    [
+                        'LLL:EXT:lang/locallang_general.xlf:LGL.allLanguages',
+                        -1,
+                        'flags-multiple'
+                    ],
+                ],
+                'default' => 0,
+            ],
+        ],
+        'l10n_parent' => [
+            'displayCond' => 'FIELD:sys_language_uid:>:0',
+            'exclude' => 1,
+            'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.l18n_parent',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [
+                    ['', 0],
+                ],
+                'foreign_table' => 'tx_ajaxselectlist_domain_model_optionrecord',
+                'foreign_table_where' => 'AND tx_ajaxselectlist_domain_model_optionrecord.pid=###CURRENT_PID### AND tx_ajaxselectlist_domain_model_optionrecord.sys_language_uid IN (-1,0)',
+            ],
+        ],
+        'l10n_diffsource' => [
+            'config' => [
+                'type' => 'passthrough',
+            ],
+        ],
 
-		't3ver_label' => array(
-			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.versionLabel',
-			'config' => array(
-				'type' => 'input',
-				'size' => 30,
-				'max' => 255,
-			)
-		),
+        't3ver_label' => [
+            'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.versionLabel',
+            'config' => [
+                'type' => 'input',
+                'size' => 30,
+                'max' => 255,
+            ]
+        ],
 
-		'hidden' => array(
-			'exclude' => 1,
-			'label' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:field.default.hidden',
-			'config' => array(
-				'type' => 'check',
-				'items' => array(
-					'1' => array(
-						'0' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:hidden.I.0'
-					)
-				)
-			),
-		),
-		'starttime' => array(
-				'exclude' => 1,
-				'l10n_mode' => 'mergeIfNotBlank',
-				'label' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:starttime_formlabel',
-				'config' => array(
-						'type' => 'input',
-						'size' => 16,
-						'max' => 20,
-						'eval' => 'datetime',
-						'default' => 0,
-				)
-		),
-		'endtime' => array(
-				'exclude' => 1,
-				'l10n_mode' => 'mergeIfNotBlank',
-				'label' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:endtime_formlabel',
-				'config' => array(
-					'type' => 'input',
-					'size' => 16,
-					'max' => 20,
-					'eval' => 'datetime',
-					'default' => 0,
-			)
-		),
-		'title' => array(
-			'exclude' => 1,
-			'label' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.title',
-			'config' => array(
-				'type' => 'input',
-				'size' => 30,
-				'eval' => 'trim,required'
-			),
-		),
-		'image' => array(
-			'exclude' => 1,
-			'label' => 'LLL:EXT:lang/locallang_tca.xlf:sys_file.type.image',
-			'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
-				'image',
-				array(
-					'appearance' => array(
-						'createNewRelationLinkTitle' => 'LLL:EXT:cms/locallang_ttc.xlf:images.addFileReference'
-					),
-					'foreign_types' => array(
-						'0' => array(
-							'showitem' => '
-							--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-							--palette--;;filePalette'
-						),
-						\TYPO3\CMS\Core\Resource\File::FILETYPE_TEXT => array(
-							'showitem' => '
-							--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-							--palette--;;filePalette'
-						),
-						\TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => array(
-							'showitem' => '
-							--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-							--palette--;;filePalette'
-						),
-						\TYPO3\CMS\Core\Resource\File::FILETYPE_AUDIO => array(
-							'showitem' => '
-							--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-							--palette--;;filePalette'
-						),
-						\TYPO3\CMS\Core\Resource\File::FILETYPE_VIDEO => array(
-							'showitem' => '
-							--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-							--palette--;;filePalette'
-						),
-						\TYPO3\CMS\Core\Resource\File::FILETYPE_APPLICATION => array(
-							'showitem' => '
-							--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-							--palette--;;filePalette'
-						)
-					),
-					'maxitems' => 1
-				),
-				$GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext']
-			),
-		),
-		'text' => array(
-			'exclude' => 1,
-			'label' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:bodytext.ALT.textbox_formlabel',
-			'config' => array(
-					'type' => 'text',
-					'cols' => '48',
-					'rows' => '5',
-					'enableRichtext' => true,
-					'richtextConfiguration' => 'default',
-			),
-			'defaultExtras' => 'richtext:rte_transform[mode=ts_css]',
-		),
+        'hidden' => [
+            'exclude' => 1,
+            'label' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:field.default.hidden',
+            'config' => [
+                'type' => 'check',
+                'items' => [
+                    '1' => [
+                        '0' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:hidden.I.0'
+                    ]
+                ]
+            ],
+        ],
+        'starttime' => [
+            'exclude' => 1,
+            'l10n_mode' => 'mergeIfNotBlank',
+            'label' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:starttime_formlabel',
+            'config' => [
+                'type' => 'input',
+                'size' => 16,
+                'max' => 20,
+                'eval' => 'datetime',
+                'default' => 0,
+            ]
+        ],
+        'endtime' => [
+            'exclude' => 1,
+            'l10n_mode' => 'mergeIfNotBlank',
+            'label' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:endtime_formlabel',
+            'config' => [
+                'type' => 'input',
+                'size' => 16,
+                'max' => 20,
+                'eval' => 'datetime',
+                'default' => 0,
+            ]
+        ],
+        'title' => [
+            'exclude' => 1,
+            'label' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.title',
+            'config' => [
+                'type' => 'input',
+                'size' => 30,
+                'eval' => 'trim,required'
+            ],
+        ],
+        'image' => [
+            'exclude' => 1,
+            'label' => 'LLL:EXT:lang/locallang_tca.xlf:sys_file.type.image',
+            'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
+                'image',
+                [
+                    'appearance' => [
+                        'createNewRelationLinkTitle' => 'LLL:EXT:cms/locallang_ttc.xlf:images.addFileReference'
+                    ],
+                    'foreign_types' => [
+                        '0' => [
+                            'showitem' => '
+                            --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+                            --palette--;;filePalette'
+                        ],
+                        \TYPO3\CMS\Core\Resource\File::FILETYPE_TEXT => [
+                            'showitem' => '
+                            --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+                            --palette--;;filePalette'
+                        ],
+                        \TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => [
+                            'showitem' => '
+                            --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+                            --palette--;;filePalette'
+                        ],
+                        \TYPO3\CMS\Core\Resource\File::FILETYPE_AUDIO => [
+                            'showitem' => '
+                            --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+                            --palette--;;filePalette'
+                        ],
+                        \TYPO3\CMS\Core\Resource\File::FILETYPE_VIDEO => [
+                            'showitem' => '
+                            --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+                            --palette--;;filePalette'
+                        ],
+                        \TYPO3\CMS\Core\Resource\File::FILETYPE_APPLICATION => [
+                            'showitem' => '
+                            --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+                            --palette--;;filePalette'
+                        ]
+                    ],
+                    'maxitems' => 1
+                ],
+                $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext']
+            ),
+        ],
+        'text' => [
+            'exclude' => 1,
+            'label' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:bodytext.ALT.textbox_formlabel',
+            'config' => [
+                'type' => 'text',
+                'cols' => '48',
+                'rows' => '5',
+                'enableRichtext' => true,
+                'richtextConfiguration' => 'default',
+            ],
+            'defaultExtras' => 'richtext:rte_transform[mode=ts_css]',
+        ],
 
-	),
-);
+    ],
+];
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,24 +1,21 @@
 <?php
-
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'Select list with Ajax call',
-	'description' => 'Provides a drop-down list which uses Ajax to load records into the page.',
-	'category' => 'plugin',
-	'author' => 'Sebastian Klein',
-	'author_email' => 'sebastian@sklein-medien.de',
-	'state' => 'beta',
-	'internal' => '',
-	'uploadfolder' => '1',
-	'createDirs' => '',
-	'clearCacheOnLoad' => 0,
-	'version' => '2.1.0',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.2.0-7.6.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-);
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Select list with Ajax call',
+    'description' => 'Provides a drop-down list which uses Ajax to load records into the page.',
+    'category' => 'plugin',
+    'author' => 'Sebastian Klein',
+    'author_email' => 'sebastian@sklein-medien.de',
+    'state' => 'beta',
+    'internal' => '',
+    'uploadfolder' => '1',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '2.1.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '6.2.0-7.6.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
