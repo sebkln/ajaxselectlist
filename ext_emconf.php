486b4f76fa1ed4fbb6d19109c36afeca70574ab8
Sebastian Klein <sebastian@sklein-medien.de>
2017-04-07 19:18:30 +0200

    Change version number and compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,10 +10,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '1',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '2.1.0',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.2.0-7.6.99',
+            'typo3' => '7.6.0-8.7.99',
         ],
         'conflicts' => [],
         'suggests' => [],
