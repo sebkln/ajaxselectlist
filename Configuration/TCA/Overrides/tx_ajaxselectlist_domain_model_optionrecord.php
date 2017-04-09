d6992a05fba1c4f7dec82bbe32e5ea38f59cf7e8
Sebastian Klein <sebastian@sklein-medien.de>
2017-04-09 21:34:42 +0200

    Unset deprecated 'defaultExtras' options for TYPO3 v8 LTS

--- Configuration/TCA/Overrides/tx_ajaxselectlist_domain_model_optionrecord.php
+++ Configuration/TCA/Overrides/tx_ajaxselectlist_domain_model_optionrecord.php
@@ -7,6 +7,7 @@ if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(
     $GLOBALS['TCA']['tx_ajaxselectlist_domain_model_optionrecord']['columns']['image']['config']['overrideChildTca']['types'] = $tca;
 
     unset($GLOBALS['TCA']['tx_ajaxselectlist_domain_model_optionrecord']['columns']['image']['config']['foreign_types']);
+    unset($GLOBALS['TCA']['tx_ajaxselectlist_domain_model_optionrecord']['columns']['text']['defaultExtras']);
 
     $GLOBALS['TCA']['tx_ajaxselectlist_domain_model_optionrecord']['columns']['image']['label'] = 'LLL:EXT:frontend/Resources/Private/Language/Database.xlf:tt_content.asset_references';
 }
