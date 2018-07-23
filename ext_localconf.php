bd89c17641072642cd315df77dad0d89dfbdf985
Sebastian Klein <sebastian@sklein-medien.de>
2018-07-23 20:12:41 +0200

    [TASK] Avoid usage of $_EXTKEY

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,15 +2,13 @@
 defined('TYPO3_MODE') or die();
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Sebkln.' . $_EXTKEY,
+    'Sebkln.ajaxselectlist',
     'Selectlist',
-    array(
+    [
         'OptionRecord' => 'list, ajaxCall',
-
-    ),
+    ],
     // non-cacheable actions
-    array(
+    [
         'OptionRecord' => '',
-
-    )
+    ]
 );
