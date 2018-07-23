1f724c4302641e7c69c703843331945aab7b1136
Sebastian Klein <sebastian@sklein-medien.de>
2018-07-23 20:10:28 +0200

    [TASK] Move TCA changes to Configuration/TCA

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -2,3 +2,9 @@
 defined('TYPO3_MODE') or die();
 
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['ajaxselectlist_selectlist'] = 'layout,select_key,linkToTop';
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'Sebkln.ajaxselectlist',
+    'Selectlist',
+    'Ajax select list'
+);
--- ext_tables.php
+++ ext_tables.php
@@ -1,13 +1,5 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'Sebkln.' . $_EXTKEY,
-    'Selectlist',
-    'Ajax select list'
-);
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Select list with Ajax call');
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_ajaxselectlist_domain_model_optionrecord', 'EXT:ajaxselectlist/Resources/Private/Language/locallang_csh_tx_ajaxselectlist_domain_model_optionrecord.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_ajaxselectlist_domain_model_optionrecord');
