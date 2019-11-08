62d4c936b86129308b978a4421bc0763bd45fd9b
Sebastian Klein <sebastian@sklein-medien.de>
2019-11-08 22:04:52 +0100

    [TASK] Restore multi-language support in TYPO3 v9

--- Classes/Controller/OptionRecordController.php
+++ Classes/Controller/OptionRecordController.php
@@ -10,6 +10,9 @@ namespace Sebkln\Ajaxselectlist\Controller;
  * LICENSE file that was distributed with this source code.
  */
 
+use TYPO3\CMS\Core\Context\Context;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 /**
  * OptionRecordController
  */
@@ -33,7 +36,16 @@ class OptionRecordController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionCon
     {
         $optionRecords = $this->optionRecordRepository->findAll();
         $this->view->assign('optionRecords', $optionRecords);
-        $this->view->assign("sysLanguageUid", $GLOBALS['TSFE']->sys_language_uid);
+        $this->view->assign("currentPageID", $GLOBALS['TSFE']->id);
+
+        // Check if the given TYPO3 uses the new language aspect (since TYPO3 9.4):
+        if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) >= 9004000) {
+            $languageAspect = GeneralUtility::makeInstance(Context::class)->getAspect('language');
+            $lang = $languageAspect->getId();
+            $this->view->assign("sysLanguageUid", $lang);
+        } else {
+            $this->view->assign("sysLanguageUid", $GLOBALS['TSFE']->sys_language_uid);
+        }
     }
 
     /**
